7f98b7443461f0cc7515747cade15ccf702284a7
Francisco <franjonas98@hotmail.com>
2024-08-26 12:14:56 -0300

    add circle draw on intersections

--- Algebra1-Grupo3/enc_temp_folder/6cb0f8a148738dd8e7c51dd5f85827e1/main.cpp
+++ Algebra1-Grupo3/enc_temp_folder/6cb0f8a148738dd8e7c51dd5f85827e1/main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 void SearchCorner(Line line[], Line corners[]);
-void lineIntersecctions(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4, Line intersections[]);
+bool lineIntersecctions(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4, Line intersections[]);
 
 int main()
 {
@@ -88,9 +88,8 @@ void SearchCorner(Line line[], Line corners[])
 	}
 }
 
-void lineIntersecctions(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4, Line intersections[])
+bool lineIntersecctions(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4, Line intersections[])
 {
-	Line auxCorner;
 
 	// calculate the distance to intersection point
 	float uA = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1));
@@ -99,13 +98,17 @@ void lineIntersecctions(float x1, float y1, float x2, float y2, float x3, float 
 	// if uA and uB are between 0-1, lines are colliding
 	if (uA >= 0 && uA <= 1 && uB >= 0 && uB <= 1)
 	{
+
 		// optionally, draw a circle where the lines meet
 		float intersectionX = x1 + (uA * (x2 - x1));
 		float intersectionY = y1 + (uA * (y2 - y1));
 
-		intersections.Intersect.x = intersectionX;
-		intersections.Intersect.y = intersectionY;
+		intersections->Intersect.x = intersectionX;
+		intersections->Intersect.y = intersectionY;
 
 		DrawCircle(intersectionX, intersectionY, 5, RED);
+
+		return true;
 	}
+	return false;
 }
