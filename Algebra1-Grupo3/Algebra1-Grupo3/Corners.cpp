a7e089f3f25555da7eff3d81368f7d41ab8552c1
Alex Gallardo <gallardonicolas.8100@outlook.com>
2024-09-26 18:05:06 -0300

    some changes done

--- Algebra1-Grupo3/Algebra1-Grupo3/Corners.cpp
+++ Algebra1-Grupo3/Algebra1-Grupo3/Corners.cpp
@@ -1,63 +1,54 @@
 #include "Corners.h"
 
-void SearchCorner(Line line[])
+void SearchCorner(Line lines[])
 {
-	Line auxLine;
+    Line auxLine;
 
-	for (int i = 0; i < LINES_AMOUNT; i++)
-	{
-		auxLine = line[i];
+    for (int i = 0; i < LINES_AMOUNT; i++)
+    {
+        auxLine = lines[i];
 
-		for (int j = 0; j < LINES_AMOUNT; j++)
-		{
-			if (auxLine.Start.x != line[j].Start.x && auxLine.Start.y != line[j].Start.y)
-			{
-				LineIntersections(auxLine.Start.x, auxLine.Start.y, auxLine.Finish.x, auxLine.Finish.y, line[j].Start.x, line[j].Start.y, line[j].Finish.x, line[j].Finish.y, line->Corners);
-			}
-		}
-	}
+        for (int j = 0; j < LINES_AMOUNT; j++)
+        {
+            if (auxLine.Start.x != lines[j].Start.x && auxLine.Start.y != lines[j].Start.y)
+            {
+                LineIntersections(auxLine.Start.x, auxLine.Start.y, auxLine.Finish.x, auxLine.Finish.y,
+                                  lines[j].Start.x, lines[j].Start.y, lines[j].Finish.x, lines[j].Finish.y, lines[j]);
+            }
+        }
+    }
 }
 
-void LineIntersections(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4, Vector2 Corners[])
+void LineIntersections(const float x1, const float y1, const float x2, const float y2, const float x3, const float y3,
+                       const float x4, const float y4, Line& line)
 {
-	Line auxCorner;
+    // calculate the distance to intersection point
+    float uA = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1));
+    float uB = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1));
 
-	// calculate the distance to intersection point
-	float uA = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1));
-	float uB = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1));
+    // if uA and uB are between 0-1, lines are colliding
+    if (uA >= 0 && uA <= 1 && uB >= 0 && uB <= 1)
+    {
+        // optionally, draw a circle where the lines meet
+        float intersectionX = x1 + (uA * (x2 - x1));
+        float intersectionY = y1 + (uA * (y2 - y1));
 
-	// if uA and uB are between 0-1, lines are colliding
-	if (uA >= 0 && uA <= 1 && uB >= 0 && uB <= 1)
-	{
-		// optionally, draw a circle where the lines meet
-		float intersectionX = x1 + (uA * (x2 - x1));
-		float intersectionY = y1 + (uA * (y2 - y1));
+        AddCorner(line, intersectionY, intersectionX);
 
-		AddCorner(Corners, intersectionY, intersectionX);
-
-		DrawCircle(intersectionX, intersectionY, 5, RED);
-	}
+        /*
+        DrawCircle(intersectionX, intersectionY, 5, RED);*/
+    }
 }
 
-void AddCorner(Vector2 Corners[], int newY, int newX)
+void AddCorner(Line& line, const int newY, const int newX)
 {
-	bool cornerDone = false;
-
-	for (int i = 0; i < MAX_CORNERS; i++)
-	{
-		if (!cornerDone)
-		{
-			Corners[i].x = newX;
-			Corners[i].y = newY;
-			cornerDone = true;
-			return;
-		}
-	}
-
+    line.Corners[0].x = newX;
+    line.Corners[0].y = newY;
+    line.CornersAmount++;
 }
 
 bool IsSameCorner(const Vector2& corner1, const Vector2& corner2)
 {
-	return corner1.x < FLT_EPSILON + corner2.x && corner1.x > corner2.x - FLT_EPSILON
-		&& corner1.y < FLT_EPSILON + corner2.y && corner1.y > corner2.y - FLT_EPSILON;
+    return corner1.x < FLT_EPSILON + corner2.x && corner1.x > corner2.x - FLT_EPSILON
+        && corner1.y < FLT_EPSILON + corner2.y && corner1.y > corner2.y - FLT_EPSILON;
 }
--- Algebra1-Grupo3/Algebra1-Grupo3/Corners.h
+++ Algebra1-Grupo3/Algebra1-Grupo3/Corners.h
@@ -2,5 +2,5 @@
 #include "Squares.h"
 
 void SearchCorner(Line line[]);
-void LineIntersections(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4, Vector2 Corners[]);
-void AddCorner(Vector2 Corners[], int newY, int newX);
+void LineIntersections(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4, const Line& line);
+void AddCorner(Line& line, int newY, int newX);
